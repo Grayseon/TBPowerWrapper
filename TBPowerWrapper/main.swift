e00fc09c69c23c4e35fbedd74a08b6d7071e3659
Grayseon <grayseon@gmail.com>
2024-10-30 08:36:10 -0500

    observers

--- TBPowerWrapper/main.swift
+++ TBPowerWrapper/main.swift
@@ -6,6 +6,45 @@
 //
 
 import Foundation
+import Cocoa
 
-print("Hello, World!")
+@discardableResult
+func shell(_ args: String...) -> Int32 {
+    let task = Process()
+    task.launchPath = "/usr/bin/env"
+    task.arguments = args
+    task.launch()
+    task.waitUntilExit()
+    return task.terminationStatus
+}
+
+class PowerObserver {
+    init(){
+        let notificationCenter = NSWorkspace.shared.notificationCenter
+        print("listening for power events")
+        
+        /*NotificationCenter.addObserver(forName: NSNotification.Name(rawValue: NSWorkspace.didWakeNotification.rawValue), object: nil, queue: .main) {notification in
+            print("woke up!")
+        }*/
+        
+        notificationCenter.addObserver(forName: NSWorkspace.willSleepNotification, object: nil, queue: .main) { notification in
+            print("going to sleep")
+        }
+        
+        notificationCenter.addObserver(forName: NSWorkspace.didWakeNotification, object: nil, queue: .main) { notification in
+            print("waking up")
+        }
+        
+        RunLoop.main.run()
+    }
+}
+
+let _ = PowerObserver()
+
+/*let desktopPath = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent("Desktop").path
+
+print(desktopPath.appending("/hey.txt"))
+
+shell("touch", desktopPath.appending("/hey.txt"))
+print(shell("whoami"))*/
 
