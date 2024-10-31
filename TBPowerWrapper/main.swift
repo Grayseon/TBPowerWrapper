d549fcfd6603c6b60ab413a2950940bb34772540
Grayseon <grayseon@gmail.com>
2024-10-31 11:35:29 -0500

    error handling

--- TBPowerWrapper/main.swift
+++ TBPowerWrapper/main.swift
@@ -8,44 +8,100 @@
 import Foundation
 import Cocoa
 
-@discardableResult
-func shell(_ args: String...) -> Int32 {
+enum ShellError: Error {
+    case processLaunchFailed
+    case processFailed
+}
+
+func shell(_ args: String..., completion: ((Int32)->(Void))?) throws {
     let task = Process()
     task.launchPath = "/usr/bin/env"
     task.arguments = args
-    task.launch()
+    do {
+        try task.run()
+    } catch {
+        throw ShellError.processLaunchFailed
+    }
     task.waitUntilExit()
-    return task.terminationStatus
+    let status = task.terminationStatus
+    
+    if status != 0 {
+        throw ShellError.processFailed
+    }
+    
+    completion?(status)
+}
+
+func unloadKext(completion: @escaping ()->()) throws {
+    do {
+        try shell("sudo", "kextunload", "/applications/tbswitcher_resources/disableturboboost.64bits.kext") {_ in
+            completion()
+        }
+    } catch {
+        print("didn't unload")
+    }
+}
+
+func loadKext(completion: @escaping ()->()) throws {
+    do {
+        try shell("sudo", "kextutil", "/applications/tbswitcher_resources/disableturboboost.64bits.kext") { status in
+            completion()
+        }
+    } catch {
+        throw ShellError.processFailed
+    }
 }
 
 class PowerObserver {
-    init(){
+    init() {
         let notificationCenter = NSWorkspace.shared.notificationCenter
         print("listening for power events")
-        self.load()
+        do {
+            try unloadKext() {
+                print("unloaded")
+                do {
+                    try loadKext() {
+                        print("successfully reloaded kext")
+                    }
+                } catch {
+                    print("unsuccessful in loading")
+                }
+            }
+        } catch {
+            print("unsuccesful in unloading")
+        }
         
         notificationCenter.addObserver(forName: NSWorkspace.willSleepNotification, object: nil, queue: .main) { notification in
             print("going to sleep")
-            self.unload()
+            try? unloadKext() {
+                print("unloaded")
+            }
         }
         
         notificationCenter.addObserver(forName: NSWorkspace.didWakeNotification, object: nil, queue: .main) { notification in
             print("woke up")
-            self.load()
+            try? loadKext() {
+                print("loaded")
+            }
         }
         
         RunLoop.main.run()
     }
-    
-    private func unload(){
-        print("unloading")
-        shell("sudo", "kextunload", "/applications/tbswitcher_resources/disableturboboost.64bits.kext")
-    }
-    
-    private func load(){
-        print("loading")
-        shell("sudo", "kextutil", "/applications/tbswitcher_resources/disableturboboost.64bits.kext")
-    }
 }
 
-let _ = PowerObserver()
+switch(CommandLine.arguments[1]) {
+case "load":
+    let _ = PowerObserver()
+case "unload":
+    do {
+        try unloadKext() {
+            print("Attemping to unload kext")
+        }
+        
+        print("Successfully unloaded kext")
+    } catch {
+        print("Unable to load kext")
+    }
+default:
+    print("No arguments")
+}
